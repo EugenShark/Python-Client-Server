76f6703b75bbf4314db369f08bc96131679ecbdc
Evgeniy Sharko <sharkoeugene@gmail.com>
2020-06-14 21:28:39 +0300

    Task1 is fixed. Task2 is done

--- task1.py
+++ task1.py
@@ -1,9 +1,9 @@
 my_list_1 = ['разработка', 'сокет', 'декоратор']
-for i in range(len(my_list_1)):
-    print(f' Переменная: {my_list_1[i]}; тип данных: {type(my_list_1[i])}')
+for i in my_list_1:
+    print(f' Переменная: {i}; тип данных: {type(i)}')
 a = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
 b = '\u0441\u043e\u043a\u0435\u0442'
 c = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
 my_list_2 = [a, b, c]
-for i in range(len(my_list_2)):
-    print(f' Переменная: {my_list_2[i]}; тип данных: {type(my_list_2[i])}')
+for i in my_list_2:
+    print(f' Переменная: {i}; тип данных: {type(i)}')
---
+++ task2.py
@@ -0,0 +1,3 @@
+my_list_0 = [b'class', b'function', b'method']
+for i in my_list_0:
+    print(f' Переменная: {i}; тип данных: {type(i)}; длина: {len(i)}')
